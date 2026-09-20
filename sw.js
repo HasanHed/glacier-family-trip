2f7f8623e6d0dfa009f2f137cf8a5e1e321e8c96
HasanHed <hedayati.che@gmail.com>
2026-09-20 18:20:31 -0400

    Refresh offline cache for Starbucks stop

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='glacier-final-v7b-20260919-routes';
+const CACHE='glacier-final-v8-20260920-coffee';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./glacier-final-pins.kml'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
